5a81d09c17731c5e800d1b6fb21ba0a4cd7cf10c
easyundercover <belenfpineyro@gmail.com>
2022-05-14 17:27:27 +0000

    cambio gitignore para que no siga .learn y ej 12

--- exercises/12-Rand-From-One-to-Twelve/app.py
+++ exercises/12-Rand-From-One-to-Twelve/app.py
@@ -1,7 +1,18 @@
 import random
 
 def get_randomInt():
-	# Your code here
+	return(random.randrange(1,12))
+
+
+print(get_randomInt())
+
+
+
+
+def get_randomInt():
+	random_number = random.randrange(1,13)
+	return random_number
+
 
 
 print(get_randomInt())
