8795f630e59421665e6601a1b45c8aaa3478d3dc
easyundercover <belenfpineyro@gmail.com>
2022-05-14 17:46:10 +0000

    ejercicio 13 loops 140522

--- exercises/12-Rand-From-One-to-Twelve/app.py
+++ exercises/12-Rand-From-One-to-Twelve/app.py
@@ -1,18 +1,6 @@
 import random
 
 def get_randomInt():
-	return(random.randrange(1,12))
-
-
-print(get_randomInt())
-
-
-
-
-def get_randomInt():
-	random_number = random.randrange(1,13)
-	return random_number
-
-
+	return(random.randrange(1,13))
 
 print(get_randomInt())
